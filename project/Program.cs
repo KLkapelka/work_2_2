b183ba88e94fbeeb7c37656d3721944ce0398f4e
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 06:07:59 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,19 @@
+﻿// Заполните массив последовательными нечетными числами, начиная с 1;
+
+using System;
+class work_2_2
+{
+    static void Main()
+    {
+        int[] array = new int[100]; 
+        int number = 1; 
+        
+        for(int i = 0; i < 100; i++)
+        {
+            array[i] = number; 
+            number += 2; 
+        }
+        var str = string.Join(" ", array); 
+        Console.WriteLine(str);
+    }
+}
